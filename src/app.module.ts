4ec03fcaaca76527020141f6f19c5094d15f289c
bentansusanto <tansusanto194@gmail.com>
2023-02-21 23:07:36 +0700

    update mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
     AuthModule,
   ],
   controllers: [AppController],
