5e63d408d14c45976201c505796123ea218adcd2
bentansusanto <tansusanto194@gmail.com>
2023-02-22 16:40:05 +0700

    update cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, 
+              {cors : {credentials : true}});
 
   app.use(cookieParser());
 
-  app.enableCors({
-    origin: 'http://localhost:8000',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:8000',
+  //   credentials: true,
+  // });
 
   await app.listen(process.env.PORT || 8000);
 }
