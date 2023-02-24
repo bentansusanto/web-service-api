c8bf30d17579ceb8201ac522935053883fd69b2b
bentansusanto <tansusanto194@gmail.com>
2023-02-24 20:27:56 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:8080',
+      origin: 'http://localhost:8000',
       credentials: true,
     },
   });
