986cf41e9d1a11218f39f7131aa7c2a96e7b1c90
bentansusanto <tansusanto194@gmail.com>
2023-02-28 12:40:51 +0700

    update jwt strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectModel(Auth.name)
     private readonly authModel: Model<Auth>,
-    private configService: ConfigService,
+    configService: ConfigService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
