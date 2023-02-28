82d2bae5633661003ea552993e50f92e9835073d
bentansusanto <tansusanto194@gmail.com>
2023-02-28 11:20:42 +0700

    update jwt strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get('JWT_KEY'),
+      secretOrKey: configService.get<string>('JWT_KEY'),
     });
   }
 
