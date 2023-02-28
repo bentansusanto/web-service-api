05d60a01fa4f502a970aec8c1bb8db601a487724
bentansusanto <tansusanto194@gmail.com>
2023-02-28 12:35:13 +0700

    update jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,25 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { Auth, AuthSchema } from './schema/auth.schema';
 import { LocalStrategy } from './local.strategy';
+import { Auth, AuthSchema } from './schema/auth.schema';
 
-const jwtFactory = {
-  useFactory: async (configService: ConfigService) => ({
-    secret: configService.get<string>('JWT_KEY'),
-    signOptions: { expiresIn: '1h' },
-  }),
-  inject: [ConfigService],
-};
+// const jwtFactory = {
+//   useFactory: async (configService: ConfigService) => ({
+//     secret: configService.get<string>('JWT_KEY'),
+//     signOptions: { expiresIn: '1h' },
+//   }),
+//   inject: [ConfigService],
+// };
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync(jwtFactory),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_KEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
     MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
   ],
   providers: [AuthService, JwtStrategy, LocalStrategy],
