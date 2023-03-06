7ef95e542f22c7899861dcb7218702d8508e8cc0
bentansusanto <tansusanto194@gmail.com>
2023-03-06 23:42:25 +0700

    update auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,7 +67,7 @@ export class AuthService {
     if (!user) {
       throw new UnauthorizedException('Invalid email or password');
     }
-    const token = this.jwtService.sign({ id: user._id });
+    const token = this.jwtService.signAsync({ id: user._id });
 
     response.cookie('jwt', token, {
       httpOnly: true,
